9b8fda8e5dde1b21d3281b6def783f7cd7108a33
fotanus@gmail.com <Felipe Tanus>
2014-12-13 07:01:07 -0200

    Hash function for classes

--- include/character/classes/class_hasher.h
+++ include/character/classes/class_hasher.h
@@ -1,11 +1,22 @@
 #pragma once
 
 namespace d20 {
+	/*
+	 * Hashes a character class using the class name.
+	 * The hash technique for strings  is called djb2
+	 * http://www.cse.yorku.ca/~oz/hash.html
+	 */
 	struct ClassHasher
 	{
 		std::size_t operator()(const Class& c) const
 		{
-			return c.get_name().size();
+			unsigned long hash = 5381;
+			const char * class_name = c.get_name().c_str();
+			int current;
+			while((current = *class_name++) != 0)
+				hash = ((hash << 5) + hash) + current;
+
+			return hash;
 		}
 	};
 }
