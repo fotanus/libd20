be9527482af30064053d43a85344298663fad012
fotanus@gmail.com <Felipe Tanus>
2014-12-13 08:00:12 -0200

    Implement operator== for character class

--- include/character/classes/class.h
+++ include/character/classes/class.h
@@ -7,6 +7,7 @@ namespace d20 {
 		std::string name;
 
 		public:
+			bool operator==(const Class &other) const;
 			virtual float bab_progression() const;
 			virtual std::string get_name() const;
     };
--- src/character/classes/class.cpp
+++ src/character/classes/class.cpp
@@ -1,11 +1,15 @@
 #include "character/classes/class.h"
 
 namespace d20 {
+	bool Class::operator==(const Class &other) const {
+		this->get_name() == other.get_name();
+	}
+
 	float Class::bab_progression() const {
 		throw("called bab_progression on class base!");
 	}
+
 	std::string Class::get_name() const {
 		throw("called get_name on class base!");
 	}
-
 }
