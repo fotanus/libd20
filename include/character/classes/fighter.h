4ed60530d5df21d84c0998f3422578cef1ed9ba5
fotanus@gmail.com <Felipe Tanus>
2014-12-13 07:23:11 -0200

    Set access level for fighter inheritance

--- include/character/classes/fighter.h
+++ include/character/classes/fighter.h
@@ -3,7 +3,7 @@
 #include "character/classes/class.h"
 
 namespace d20 {
-	class Fighter : Class {
+	class Fighter : public Class {
 		public:
 			float bab_progression() const override;
 			std::string get_name() const override;
