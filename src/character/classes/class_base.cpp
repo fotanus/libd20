62aa0c5ee3881a89c257760aa0fc18e47562a7f3
fotanus@gmail.com <Felipe Tanus>
2014-12-13 06:45:52 -0200

    class base will not be pure virtual

--- src/character/classes/class_base.cpp
+++ src/character/classes/class_base.cpp
@@ -1,2 +1,11 @@
+#include "character/classes/class_base.h"
+
 namespace d20 {
+	float ClassBase::bab_progression() const {
+		throw("called bab_progression on class base!");
+	}
+	std::string ClassBase::get_name() const {
+		throw("called get_name on class base!");
+	}
+
 }
